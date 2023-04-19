1e8229677d66036293827a5df5dba0ac56d28d63
Alicia <aliciavm@hotmail.fr>
2023-04-19 14:46:50 +0200

    deploy

---
+++ components/Home.js
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import { Popover, Button } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import Movie from "./Movie";
+import "antd/dist/antd.css";
+import styles from "../styles/Home.module.css";
+
+function Home() {
+  const [likedMovies, setLikedMovies] = useState([]);
+
+  // Liked movies (inverse data flow)
+  const updateLikedMovies = (movieTitle) => {
+    if (likedMovies.find((movie) => movie === movieTitle)) {
+      setLikedMovies(likedMovies.filter((movie) => movie !== movieTitle));
+    } else {
+      setLikedMovies([...likedMovies, movieTitle]);
+    }
+  };
+
+  const likedMoviesPopover = likedMovies.map((data, i) => {
+    return (
+      <div key={i} className={styles.likedMoviesContainer}>
+        <span className="likedMovie">{data}</span>
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          onClick={() => updateLikedMovies(data)}
+          className={styles.crossIcon}
+        />
+      </div>
+    );
+  });
+
+  const popoverContent = (
+    <div className={styles.popoverContent}>{likedMoviesPopover}</div>
+  );
+
+  // Movies list
+  const API_KEY = process.env.API_KEY;
+  const [moviesData, setMoviesData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.movies);
+        setMoviesData(data.movies);
+      });
+  }, []);
+
+  const movies = moviesData.map((movies, i) => {
+    console.log("movies title : ", movies.vote_average);
+    const isLiked = likedMovies.some((movie) => movie === movies.title);
+    return (
+      <Movie
+        key={i}
+        updateLikedMovies={updateLikedMovies}
+        isLiked={isLiked}
+        title={movies.title}
+        overview={
+          movies.overview.length > 250
+            ? `${movies.overview.substring(0, 250)}...`
+            : movies.overview
+        }
+        poster={`https://image.tmdb.org/t/p/w500/${movies.poster_path}`}
+        voteAverage={movies.vote_average}
+        voteCount={movies.vote_count}
+      />
+    );
+  });
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <div className={styles.logocontainer}>
+          <img src="logo.png" alt="Logo" />
+          <img className={styles.logo} src="logoletter.png" alt="Letter logo" />
+        </div>
+        <Popover
+          title="Liked movies"
+          content={popoverContent}
+          className={styles.popover}
+          trigger="click"
+        >
+          <Button>♥ {likedMovies.length} movie(s)</Button>
+        </Popover>
+      </div>
+      <div className={styles.title}>LAST RELEASES</div>
+      <div className={styles.moviesContainer}>{movies}</div>
+    </div>
+  );
+}
+
+export default Home;
