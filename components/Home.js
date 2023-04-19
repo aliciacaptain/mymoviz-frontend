50206c32bb7b9c57d0eb6e952f847a36c09a0dc3
Alicia <aliciavm@hotmail.fr>
2023-04-19 14:58:52 +0200

    final

--- components/Home.js
+++ components/Home.js
@@ -39,7 +39,7 @@ function Home() {
   const API_KEY = process.env.API_KEY;
   const [moviesData, setMoviesData] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:3000/movies")
+    fetch("mymoviz-backend-lgqxmh347-aliciacaptain.vercel.app")
       .then((response) => response.json())
       .then((data) => {
         console.log(data.movies);
